2b91b3c72e4455ab9a44a68322d554bd53f6cbaf
Folasayo Samuel <folasayosamuel@gmail.com>
2022-09-15 16:47:27 +0100

    update

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace FullFunctionCalculator.Cal
+{
+	public class Calculator
+	{
+		public static void MyCal()
+		{
+			Console.Write("Enter a number: ");
+			double num1 = Convert.ToDouble(Console.ReadLine());
+
+			Console.Write("Enter Operator: ");
+			string op = Console.ReadLine();
+
+			Console.Write("Enter a number: ");
+			double num2 = Convert.ToDouble(Console.ReadLine());
+			
+			if(op == "+")
+			{
+                Console.Write(num1 + num2);
+            }else if(op == "-")
+			{
+                Console.WriteLine(num1 - num2);
+            }else if(op == "*")
+			{
+                Console.WriteLine(num1 * num2);
+            }else if(op == "/")
+			{
+                Console.WriteLine(num1 / num2);
+            }else
+			{
+                Console.WriteLine("Invalid Input!");
+            }
+			
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FullFunctionCalculator.Cal;
+
+namespace FullFunctionCalculator
+{
+	class Program
+	{
+		 static void Main(string[]args)
+		{
+            Calculator.MyCal();
+
+            Console.ReadLine();
+		}
+		
+		
+	}
+}
